0d86e905eb03c4d90222191186a774c69c119e1a
Alex <alexperez689@gmail.com>
2023-02-23 20:03:33 -0500

    error file added

---
+++ error.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLayout>
+    <div class="prose">
+      <h1>Sheeeeeesh!</h1>
+      <p>Looks like you broke something.</p>
+      <p>Lets see if we can fix it.</p>
+      <p>
+        <strong class="text-red-500">{{ error.message }}</strong>
+      </p>
+      <p>
+        Go back to the
+        <a
+          class="hover:cursor-pointer"
+          @click="handleError"
+        >
+          first lesson.
+        </a>
+      </p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+  const error = useError();
+  const handleError = () => {
+    clearError({
+      redirect:
+      '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3',
+    });
+  }
+</script>
